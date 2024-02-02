923363caa8cab8745f10547901f89cde9a135ab5
princesudani <princesudani0610@gmail.com>
2024-02-02 18:15:10 +0530

    Error Page

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Button } from './styles/Button'
+import { NavLink } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
+    <Wrapper>
+    <div className="container">
     <div>
-      Error page
+      <h2>404</h2>
+      <h2>UH OH !!! You're Lost.</h2>
+      <p>The Page you are Looking for does not exist. How you got hare is a mystery. But you can click the button below to go back to the Homepage</p>
+      <Button>
+        <NavLink to="/">Go Back to Homepage</NavLink>
+      </Button>
     </div>
+    </div>
+    </Wrapper>
   )
 }
+const Wrapper = styled.section`
+  .container {
+    padding: 9rem 0;
+    text-align: center;
+
+    h2 {
+      font-size: 10rem;
+    }
+
+    h3 {
+      font-size: 4.2rem;
+    }
+
+    p {
+      margin: 2rem 0;
+    }
+  }
+`;
 
 export default ErrorPage
