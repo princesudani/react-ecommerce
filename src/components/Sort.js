9d2db693dc89de83630fc3b87aa065caa1cd0542
princesudani <princesudani0610@gmail.com>
2024-02-09 18:57:18 +0530

    Sorting Added

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,7 +1,90 @@
 import React from "react";
+import styled from "styled-components";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaList } from "react-icons/fa";
+import { useFilterContext } from "../context/filter_context";
 
 const Sort = () => {
-  return <div>Sort</div>;
+  const { filter_products, grid_view, setGridView, setListView } =
+    useFilterContext();
+  return (
+    <Wrapper className="sort-section">
+      {/* 1st column */}
+      <div className="sorting-list--grid">
+        <button
+          onClick={setGridView}
+          className={grid_view ? "active sort-btn" : "sort-btn"}
+        >
+          <BsFillGridFill className="icon" />
+        </button>
+
+        <button
+          onClick={setListView}
+          className={!grid_view ? "active sort-btn" : "sort-btn"}
+        >
+          <FaList className="icon" />
+        </button>
+      </div>
+      {/* 2nd columns */}
+      <div className="product-data">
+        <p>{`${filter_products.length} Availabel Products`}</p>
+      </div>
+      {/* 3rd column  */}
+      <div className="sort-selection">
+        <form action="#">
+          <label htmlFor="sort"></label>
+          <select name="sort" id="sort" className="sort-selection--style">
+            <option value="lowest">Price(lowest)</option>
+            <option value="#" disabled></option>
+            <option value="highest">Price(highest)</option>
+            <option value="#" disabled></option>
+            <option value="a-z">Price(a-z)</option>
+            <option value="#" disabled></option>
+            <option value="z-a">Price(z-a)</option>
+          </select>
+        </form>
+      </div>
+    </Wrapper>
+  );
 };
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5rem;
+
+  .sorting-list--grid {
+    display: flex;
+    gap: 2rem;
+
+    .sort-btn {
+      padding: 0.8rem 1rem;
+      border: none;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+    }
+
+    .icon {
+      font-size: 1.6rem;
+    }
+    .active {
+      background-color: ${({ theme }) => theme.colors.black};
+      color: #fff;
+    }
+  }
+
+  .sort-selection .sort-selection--style {
+    padding: 0.5rem;
+    cursor: pointer;
+
+    .sort-select--option {
+      padding: 0.5rem 0;
+      cursor: pointer;
+      height: 2rem;
+      padding: 10px;
+    }
+  }
+`;
 
 export default Sort;
--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -13,6 +13,12 @@ const filterReducer = (state, action) => {
         grid_view: true,
       };
 
+    case "SET_LIST_VIEW":
+      return {
+        ...state,
+        grid_view: false,
+      };
+
     default:
       return state;
   }
